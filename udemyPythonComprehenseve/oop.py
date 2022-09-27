e41c2fcffb5d7332a4ee9d3567e6d170eaac3999
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-09-27 14:16:38 +0200

    modifying classed, adding attributes

--- udemyPythonComprehenseve/oop.py
+++ udemyPythonComprehenseve/oop.py
@@ -1,19 +1,23 @@
 # object-oriented programming
 # creating a class
+from cgi import print_directory
 from platform import system_alias
 
 ## OOP Seciton 11
 class instructors:
-    companyName= "Linux OpenSource Development"
+    companyName= "TECH ACADEMY"
     
-    def __init__(self,course): # __init__  --> is the initializer
+    def __init__(self,courseName, duration): # __init__  --> is the initializer
         # (self) parameter is used to aceess the variables and methods
         # that belongs to the class
-        self.course = course
+        self.courseName = courseName
+        self.duration = duration
 
-    def printinfo(self):
+    def printCourseInfo(self):
         print("My Company Name is: ", instructors.companyName)
-
+        # print("The Course Name: ", instructors.courseName)
+        # print("The Course Duration: ", instructors.duration)
+        # print("\n")
 
 # a class with no methods
 class Pets:
@@ -21,9 +25,19 @@ class Pets:
 
 
 # instantiating object of the class
-elearning = instructors("Becoming a linux developer") 
-bls = instructors("Django for begineers")
-elearning.printinfo()
+# elearning = instructors("Becoming a linux developer",7) 
+# bls = instructors("Django for begineers")
+# elearning.printCourseInfo()
+ # will print the value of the course attribute
+# print(elearning.courseName)
+# print(elearning.duration)
 
-print(elearning.course) # will print the value of the course attribute
 
+# modifying classed, adding attributes
+# adding duration attribute to instructors class
+courseInWebsite = instructors("Introduction To FreeLancing", "8 Hours")
+courseInWebsite.courseName= "Linux Machines Decoding"
+courseInWebsite.duration= "30 Hours"
+courseInWebsite.printCourseInfo()
+print(courseInWebsite.courseName)
+print(courseInWebsite.duration)
