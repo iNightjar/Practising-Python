3f7d0583ace29fcf250a6ac8c8845eb7fc130028
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-09-30 07:53:05 +0200

    Guess number generated by computer

---
+++ udemyPythonComprehenseve/numberGuessingGame.py
@@ -0,0 +1,25 @@
+from http.client import ImproperConnectionState
+
+import random
+
+guesses = []
+
+myComputer = random.randint(1,70)
+
+player = int(input("Enter a number between 1-70--> "))
+guesses.append(player)
+
+while player != myComputer:
+    if player > myComputer:
+        print("number is too high")
+    else:
+        print("Number is too low: ")
+    player = int(input("Enter a number between 1-70: "))
+    guesses.append(player)
+
+
+else:
+    print("You have guessed right: good job!")
+    print("It took you %i guesses. " % len(guesses))
+    print("These were your guesses: ")
+    print(guesses)
